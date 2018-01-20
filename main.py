d1f0a88846cf542e89a9eb29e46672af17fb44ec
Bradon-Lodwick <bradonlodwick22@gmail.com>
2018-01-19 21:03:03 -0500

    Changed to discord.py rewrite
    Finalized the create_group and add_to_group functions for the

--- main.py
+++ main.py
@@ -33,33 +33,38 @@ async def create_group(ctx, name):
     :param name: The desired name for the group. Will be role name and prefix the chats.
     :type name: str
     """
-    # The server the message was sent in
-    server = ctx.message.server
+    # The guild the message was sent in
+    guild = ctx.message.guild
+    # The author of the message
+    author = ctx.message.author
+    # The channel the message was sent in
+    channel = ctx.message.channel
 
-    # Creates the group role
-    group_role = await bot.create_role(server, name=name, mentionable=True)
-    # Adds the author to the new role
-    await bot.add_roles(ctx.message.author, group_role)
+    # Checks if the name only has alphabetic
+    if name.isalpha():
+        # Creates the group role
+        group_role = await guild.create_role(name=name, mentionable=True)
+        # Adds the author to the new role
+        await author.add_roles(group_role)
 
-    # Setup the permissions to be passed to the channels
-    everyone_perm = discord.PermissionOverwrite(read_messages=False)
-    group_perm = discord.PermissionOverwrite(read_messages=True)
+        # Setup the permissions to be passed to the channels
+        everyone_perm = discord.PermissionOverwrite(read_messages=False)
+        group_perm = discord.PermissionOverwrite(read_messages=True)
+        permissions = {guild.default_role: everyone_perm, group_role: group_perm}
 
-    # Creates the important text channel
-    await bot.create_channel(
-        ctx.message.server, "{}-important".format(name),
-        (ctx.message.server.default_role, everyone_perm), (group_role, group_perm), type=discord.ChannelType.text
-    )
-    # Creates the general text channel
-    await bot.create_channel(
-        ctx.message.server, "{}-general".format(name),
-        (ctx.message.server.default_role, everyone_perm), (group_role, group_perm), type=discord.ChannelType.text
-    )
-    # Creates the general voice chat
-    await bot.create_channel(
-        ctx.message.server, "{}-general".format(name),
-        (ctx.message.server.default_role, everyone_perm), (group_role, group_perm), type=discord.ChannelType.voice
-    )
+        # Creates the category channel for the group
+        category = await guild.create_category(name, overwrites=permissions)
+        # Creates the important text channel for the group
+        await guild.create_text_channel("{}-important".format(name), category=category)
+        # Creates the general text channel for the group
+        await guild.create_text_channel("{}-text".format(name), category=category)
+        # Creates the voice channel for the group
+        await guild.create_voice_channel("{}-voice".format(name), category=category)
+
+        # Send a success message after completing making the channels and role
+        await channel.send(content="The group has been successfully created.")
+    else:
+        await channel.send(content="Use only letters as the group name.")
 
 
 # Adds a user to a group, if the user running the command is in the group
@@ -69,17 +74,19 @@ async def add_to_group(ctx):
 
     :param ctx: The context of the message.
     """
-    # Gets the author of the message.
+    # Gets the author of the message
     author = ctx.message.author
+    # Gets the channel the message was send in
+    channel = ctx.message.channel
     # Checks to see that only 1 role was mentioned
     if len(ctx.message.role_mentions) != 1:
-        await bot.say("Please mention the role that you would like to add the users to.")
+        await channel.send(content="Please mention the role (only 1 role) that you would like to add the users to.")
     # Checks to see if the role mentioned is in the user's role list
     elif ctx.message.role_mentions[0] not in author.roles:
-        await bot.say("You must belong to the role in order to add users to it.")
+        await channel.send(content="You must belong to the role in order to add users to it.")
     # Checks to see if the user mentioned any users
     elif len(ctx.message.mentions) < 1:
-        await bot.say("Please mention the users that you would like to have added to the role.")
+        await channel.send(content="Please mention the users that you would like to have added to the role.")
     # Adds the users to the given role, as all security checks have passed
     else:
         # Try to add the users to the role
@@ -89,11 +96,11 @@ async def add_to_group(ctx):
                 # Checks if the user is already in the role
                 if ctx.message.role_mentions[0] not in user.roles:
                     # Adds the role to the user
-                    await bot.add_roles(user, ctx.message.role_mentions[0])
-            await bot.say("The given users have been successfully added to the given role.")
+                    await user.add_roles(ctx.message.role_mentions[0])
+            await channel.send(content="The given users have been successfully added to the given role.")
         # Exceptions in the case of the bot not having permissions
         except discord.Forbidden:
-            await bot.say("I don't have permissions to give user roles.")
+            await channel.send(content="I don't have permissions to give user roles.")
 
 
 # Runs the bot
